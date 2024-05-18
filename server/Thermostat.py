9159bf270aeabf78d3b172acae651d79b20614a7
Jack Wesolowski <78510801+jackw2@users.noreply.github.com>
2024-05-17 22:46:50 -0400

    can login and get data from thermostat

--- server/Thermostat.py
+++ server/Thermostat.py
@@ -1,6 +1,7 @@
 from venstarcolortouch import VenstarColorTouch
 import json
 
+
 class Thermostat:
   _instance = None
 
@@ -15,10 +16,29 @@ class Thermostat:
     with open("config.json", "r") as file:
       config = json.load(file)
       self.device_info = config['device_info']
-    self.device = VenstarColorTouch(device_info["ip_address"], timeout=device_info["timeout"])
-    self.device.login()
-    self.device.update_info()
+    self.device = VenstarColorTouch(
+      self.device_info["ip_address"],
+      timeout=self.device_info["timeout"]
+    )
 
-  def get_data(self):
+  def login(self) -> bool:
+    if self.device.login() is True:
+      print("Login successful. API: {0} Type: {1}".format(
+        self.device._api_ver, self.device._type))
+      return True
+    else:
+      print("Login failed.")
+      return False
+
+  def get_data(self) -> dict:
     # Add your code here to fetch and return the thermostat data
-    pass
+    if self.device.update_info() is True:
+      data = {
+          "state": self.device.get_info("state"),
+          "away": self.device.get_info("away"),
+          "spacetemp": self.device.get_info("spacetemp"),
+          "heattemp": self.device.get_info("heattemp"),
+          "cooltemp": self.device.get_info("cooltemp")
+      }
+      return data
+    return None
--- server/thermo.py
+++ server/thermo.py
@@ -1,26 +1,7 @@
 # Author: Jack Wesolowski
 # https://github.com/jackw2
 
+from Thermostat import Thermostat
 
-
-# Read from config.jsonn
-
-ct = VenstarColorTouch(device_info["ip_address"], timeout=5)
-
-if ct.login() is True:
-  print("Login successful. API: {0} Type: {1}".format(ct._api_ver,ct._type))
-
-if ct.update_info() is True:
-  print("Was able to get info:{0}".format(ct._info))
-else:
-  print("Was not able to get info")
-
-data = {
-  "state": ct.get_info("state"),
-  "away": ct.get_info("away"),
-  "spacetemp": ct.get_info("spacetemp"),
-  "heattemp": ct.get_info("heattemp"),
-  "cooltemp": ct.get_info("cooltemp")
-}
-print(data)
-
+Thermostat().login()
+print(Thermostat().get_data())
