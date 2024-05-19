0a3383f497d9410b9560d179af74e3a27f3d7938
Jack Wesolowski <78510801+jackw2@users.noreply.github.com>
2024-05-19 00:20:46 -0400

    init iphone & watch apps

---
+++ thermostat/thermostat Watch App/thermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  thermostatApp.swift
+//  thermostat Watch App
+//
+//  Created by Jack Wesolowski on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct thermostat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ thermostat/thermostat/thermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  thermostatApp.swift
+//  thermostat
+//
+//  Created by Jack Wesolowski on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct thermostatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
