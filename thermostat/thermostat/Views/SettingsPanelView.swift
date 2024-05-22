d812250ce9c312de3661e4f361683b464d730eb6
Jack Wesolowski <78510801+jackw2@users.noreply.github.com>
2024-05-22 00:06:59 -0400

    reorganize project files hierarchy

---
+++ thermostat/thermostat/Views/SettingsPanelView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsPanel.swift
+//  thermostat
+//
+//  Created by Jack Wesolowski on 5/19/24.
+//
+
+import SwiftUI
+
+struct SettingsPanelView: View {
+
+    var body: some View {
+        Form {
+            Text("hello")
+        }
+    }
+}
+
+#Preview {
+    SettingsPanelView()
+}
