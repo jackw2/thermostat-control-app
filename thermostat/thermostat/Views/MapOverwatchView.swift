a633c4e9700202c50e116d087fc8f3f0444bde08
Jack Wesolowski <78510801+jackw2@users.noreply.github.com>
2024-06-03 01:47:42 -0400

    add range circles to map overwatch

--- thermostat/thermostat/Views/MapOverwatchView.swift
+++ thermostat/thermostat/Views/MapOverwatchView.swift
@@ -5,44 +5,39 @@
 //  Created by Jack Wesolowski on 6/3/24.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
 
-//struct MapPanelView: View {
-//    @State private var homeAddress = CLLocationCoordinate2D(latitude: 39.947039, longitude: -82.980115)
-//    @State private var locationManager = CLLocationManager()
-//
-//    var body: some View {
-//        Map() {
-//                    Annotation("Parking", coordinate: .parking) {
-//                        ZStack {
-//                            RoundedRectangle(cornerRadius: 5)
-//                                .fill(.background)
-//                            RoundedRectangle(cornerRadius: 5)
-//                                .stroke(.secondary, lineWidth: 5)
-//                            Image(systemName: "car")
-//                                .padding(5)
-//                        }
-//                    }
-//                    .annotationTitles(.hidden) // hide the title, icon only
+struct MapOverwatchView: View {
+    var locationManager = CLLocationManager()
+    @State private var cameraPosition: MapCameraPosition = .automatic
+    @AppStorage("homeTitle") private var homeTitle: String = "The White House"
+    @AppStorage("homeLatitude") private var homeLatitude: Double = 38.897957
+    @AppStorage("homeLongitude") private var homeLongitude: Double = -77.036560
+    @State private var homeAddress:CLLocationCoordinate2D? = nil
+    
+    
+    var body: some View {
+        Map(position: $cameraPosition) {
+            UserAnnotation()
+            if let homeAddress = homeAddress {
+                Marker(homeTitle, coordinate: homeAddress)
+                    .tint(.blue)
+                MapCircle(center: homeAddress, radius: 1000)
+                    .stroke(.blue.opacity(0.5), lineWidth: 5)
+                    .foregroundStyle(.white.opacity(0.2))
+                    .mapOverlayLevel(level: .aboveLabels)
+            }
+        }
+        .mapStyle(.standard(elevation: .flat, emphasis: .muted, pointsOfInterest: .excludingAll, showsTraffic: false))
+        .onAppear() {
+            locationManager.requestAlwaysAuthorization()
+            homeAddress = CLLocationCoordinate2D(latitude: homeLatitude, longitude: homeLongitude)
+        }
+    }
+}
 
-//        VStack {
-//            Map {
-//                Marker("Home", coordinate: homeAddress)
-//                    .tint(.blue)
-//                MapCircle(center: homeAddress, radius: 1000)
-//                    .stroke(.blue.opacity(0.5), lineWidth: 5)
-//                    .foregroundStyle(.white.opacity(0.2))
-//                    .mapOverlayLevel(level: .aboveLabels)
-//
-//            }
-//            .mapControls {
-//                MapUserLocationButton()
-//            }
-//            .mapControlVisibility(.hidden)
-//            .mapStyle(.standard(elevation: .flat, emphasis: .muted, pointsOfInterest: .excludingAll, showsTraffic: false))
-//        }
-//        .onAppear {
-//            locationManager.requestWhenInUseAuthorization()
-//        }
-//    }
-//}
+#Preview {
+    MapOverwatchView()
+}
+
