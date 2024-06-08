38ca7859b405967acfa18fc565315718b93980e0
Jack Wesolowski <78510801+jackw2@users.noreply.github.com>
2024-06-08 16:32:35 -0400

    update map to use new settings model

--- thermostat/thermostat/map/MapOverwatchView.swift
+++ thermostat/thermostat/map/MapOverwatchView.swift
@@ -8,50 +8,42 @@
 import SwiftUI
 import MapKit
 struct MapOverwatchView: View {
-    var locationManager = CLLocationManager()
     @State private var cameraPosition: MapCameraPosition = .automatic
-    @AppStorage("homeTitle") private var homeTitle: String = "The White House"
-    @AppStorage("homeLatitude") private var homeLatitude: Double = 38.897957
-    @AppStorage("homeLongitude") private var homeLongitude: Double = -77.036560
-    @State private var homeAddress:CLLocationCoordinate2D? = nil
-
-    @Environment(SettingsModelOld.self) private var settingsModel
-    private var radiusInMeters: Double {
-        let radiusInMiles = Measurement<UnitLength>(value: Double(settingsModel.homeRadius), unit: .miles)
-        return radiusInMiles.converted(to: .meters).value
-    }
     
+    @EnvironmentObject var settings: SettingsModel
+    var locationManager = CLLocationManager()
+
     var body: some View {
-        @Bindable var settingsModel = settingsModel
+        let homeCoords = CLLocationCoordinate2D(latitude: settings.homeLatitude, longitude: settings.homeLongitude)
         
-        Map(position: $cameraPosition) {
+        return Map(position: $cameraPosition) {
             UserAnnotation()
-            if let homeAddress = homeAddress {
-                Marker(homeTitle, coordinate: homeAddress)
-                    .tint(.blue)
-                MapCircle(center: homeAddress, radius: radiusInMeters)
-                    .stroke(.blue.opacity(0.5), lineWidth: 5)
-                    .foregroundStyle(.white.opacity(0.2))
-                    .mapOverlayLevel(level: .aboveLabels)
+            if let lastCoord = locationManager.location?.coordinate {
+                Marker("Last Updated Location", coordinate: locationManager.location!.coordinate)
             }
+            
+            Marker(settings.homeTitle, coordinate: homeCoords)
+                .tint(.blue)
+            MapCircle(center: homeCoords, radius: settings.homeRadiusInMeters)
+                .stroke(.blue.opacity(0.5), lineWidth: 5)
+                .foregroundStyle(.white.opacity(0.2))
+                .mapOverlayLevel(level: .aboveLabels)
         }
         .mapStyle(.standard(elevation: .flat, emphasis: .muted, pointsOfInterest: .excludingAll, showsTraffic: false))
         .onAppear() {
             locationManager.requestAlwaysAuthorization()
-            homeAddress = CLLocationCoordinate2D(latitude: homeLatitude, longitude: homeLongitude)
         }
     }
 }
 
 #Preview {
     struct PreviewWrapper: View {
-        @State private var settingsModel = SettingsModelOld.standard
+        @StateObject private var settings = SettingsModel.shared
         
         var body: some View {
             MapOverwatchView()
-                .environment(settingsModel)
+                .environmentObject(settings)
         }
     }
     return PreviewWrapper()
 }
-
